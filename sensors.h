dc90abaf867c4e603ffa73185506717595d3a67d
rageoveralostpenny <wu.cindyx@gmail.com>
2020-11-19 20:38:55 +0000

    added output for light sensors

--- sensors.h
+++ sensors.h
@@ -16,13 +16,35 @@ float readRightUS();
 float readFrontUS();
 
 // Function to read values of the LEFT light sensor
-// Output true/false if it finds the line or not (can tuen on the sensor it self)
+// Output true/false if it finds the line or not (can turn on the sensor itself)
 float readLeftLight();
+  bool val = digitalRead(Sensor);
+  if{val ==1}{
+    return true
+  }
+  else{
+    return false
+  }
+  delay(500);
+  
+  
 // Function to read values of the RIGHT light sensor
-// Output true/false if it finds the line or not (can tuen on the sensor it self)
+// Output true/false if it finds the line or not (can turn on the sensor itself)
 float readRightLight();
-
+  if{val ==1}{
+    return true
+  }
+  else{
+    return false
+  }
+  delay(500);
+  
 // Quick print function to see if all the ultrasonic sensors are funcitonal 
 void displayUS();
 // Quick print function to see if all the light sensors are funcitonal 
 void displayLight();
+  l_light = readRightlight();
+  r_light = readRightLight();
+  cout <<l_light;
+  cout <<r_light;
+  cout readLeftLight();
